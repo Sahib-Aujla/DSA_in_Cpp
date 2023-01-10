007481d039b219c66918b755fa186f3d055eb657
Sahib-Aujla <sahibpreet568@gmail.com>
2023-01-10 13:26:24 -0500

    Adding vectors

---
+++ vectors/vector1.cpp
@@ -0,0 +1,26 @@
+//finding the last occurence of an element
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int lastOccurence(vector<int> &v,int x){
+    int index=-1;
+    for(int i=0;i<v.size();i++){
+        if(v[i]==x){
+            index=i;
+        }
+    }
+    return index;
+}
+
+
+int main()
+{
+
+    vector <int> v={1,2,3,4,6,7,3,2,5,1,3,5,3,6,1};
+    int index=lastOccurence(v,1);
+cout<<"last occurence  "<<index<<endl;
+return 0;
+}
