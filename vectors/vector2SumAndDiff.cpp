bd1058faac4641418a3adac9ce38f08bc57d94ae
Sahib-Aujla <sahibpreet568@gmail.com>
2023-01-10 17:47:26 -0500

    difference between Sum of even indices and odd indices

---
+++ vectors/vector2SumAndDiff.cpp
@@ -0,0 +1,29 @@
+//difference between Sum of even indices and odd indices
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+    vector <int> v={1,2,1,2,1,2,1,3};
+   int evenSum=0;
+   int oddSum=0;
+    for(int i=0; i<v.size(); i+=2){
+        if(i+1<v.size()){
+            
+            oddSum+=v[i+1];
+        }
+        evenSum+=v[i];
+    }
+
+cout<<"evenSum  "<<evenSum<<"  oddSum "<<oddSum<<endl;
+
+
+    return 0;
+}
